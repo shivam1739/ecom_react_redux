a271142f8d366edffb2b59751a44a303b1d589f7
shivam1739 <patelshivam1739@gmail.com>
2023-05-08 15:04:45 +0530

    header edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
-import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
@@ -12,37 +11,38 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import NotFoundScreen from "./screens/NotFoundScreen";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <main className="py-3">
-          <Switch>
-            <Route path="/" exact component={<LoginScreen />} />
-            <Route path="/home" component={<HomeScreen />} />
-            <Route path="/search/:keyword" exact component={<HomeScreen />} />
-            <Route path="/page/:pageNumber" exact component={<HomeScreen />} />
-            <Route
-              path="/search/:keyword/page/:pageNumber"
-              exact
-              component={<HomeScreen />}
-            />
-            <Route path="/product/:id" component={<ProductScreen />} />
-            <Route path="/cart/:id?" component={<CartScreen />} />
-            <Route path="/register" component={<RegisterScreen />} />
-            <Route path="/shipping" component={<ShippingScreen />} />
-            <Route path="/payment" component={<PaymentScreen />} />
-            <Route path="/placeorder" component={<PlaceOrderScreen />} />
-            <Route path="/order/:id" component={<OrderScreen />} />
-            <Route path="/profile" component={<ProfileScreen />} />
-            <Route path="*" component={<NotFoundScreen />} />
-            <Route path="/profile/edit/:id" component={<ProfileScreen />} />
-          </Switch>
-        </main>
+
+        <Routes>
+          <Route path="/" element={<SigninScreen />} />
+          <Route path="/home" element={<HomeScreen />} />
+          <Route path="/search/:keyword" element={<HomeScreen />} />
+          <Route path="/page/:pageNumber" element={<HomeScreen />} />
+          <Route
+            path="/search/:keyword/page/:pageNumber"
+            exact
+            element={<HomeScreen />}
+          />
+          <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/cart/:id?" element={<CartScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/shipping" element={<ShippingScreen />} />
+          <Route path="/payment" element={<PaymentScreen />} />
+          <Route path="/placeorder" element={<PlaceOrderScreen />} />
+          <Route path="/order/:id" element={<OrderScreen />} />
+          <Route path="/profile" element={<ProfileScreen />} />
+          <Route path="*" element={<NotFoundScreen />} />
+          <Route path="/profile/edit/:id" element={<ProfileScreen />} />
+        </Routes>
+
         <Footer />
       </Router>
     </div>
---
+++ src/screens/NotFoundScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundScreen = () => {
+  return (
+    <div>
+      <h1>not found</h1>
+    </div>
+  );
+};
+
+export default NotFoundScreen;
