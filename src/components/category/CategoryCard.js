540fdd08dd5d1daf7de73a767b39ccb5c070db30
shivam1739 <patelshivam1739@gmail.com>
2023-05-27 15:43:49 +0530

    categroy section add

--- src/components/category/CategoryCard.js
+++ src/components/category/CategoryCard.js
@@ -4,18 +4,42 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
+import Button from "../button/Button";
+
+// import { Button } from "bootstrap";
 
 const CategoryCard = (props) => {
   return (
-    <Container>
-      <Row>
+    <Container className={"bg-dark-subtle"}>
+      <Row className="">
         {props.categories?.map((category, index) => (
-          <Col key={index} sm={4}>
+          <Col
+            key={index}
+            sm={4}
+            className="cat-card position-relative d-flex  justify-content-center align-items-center"
+          >
             <Image
               src={category.image}
               alt={category.name}
-              className="img-fluid"
+              className="img-fluid d-flex justify-content-center align-items-center rounded position-arelative"
             />
+            <div className="card-name none">
+              <Button
+                props={{
+                  name: category.name,
+                  style: {
+                    width: "30%",
+                    height: "2.5rem",
+                    backgroundColor: "#E0EFF6",
+                    color: " #07484A",
+                    fill: "solid",
+                    fontWeight: "bolder",
+                    borderRadius: "5px",
+                    border: "none",
+                  },
+                }}
+              />
+            </div>
           </Col>
         ))}
       </Row>
---
+++ src/components/seperator/Seperator.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Seperator = (props) => {
+  return (
+    <div className="bg-dark text-white p-2 mb-1">
+      <h2>{props.name}</h2>
+    </div>
+  );
+};
+
+export default Seperator;
